4e5bd8c51d62d9d9fbd8caa88daf7cce114862c2
Nhan Tran <138663806+NhanTran262@users.noreply.github.com>
2025-03-19 20:23:26 +0700

    Update

--- src/components/ui/SlideHoverButton.jsx
+++ src/components/ui/SlideHoverButton.jsx
@@ -1,4 +1,5 @@
 import { Button } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
 
 export default function SlideHoverButton({ children, className, bgButton, bgHover, textColor, textHover, ...props }) {
   console.log(textHover)
@@ -6,13 +7,23 @@ export default function SlideHoverButton({ children, className, bgButton, bgHove
   return (
     <>
       <Button
-        className={`relative overflow-hidden cursor-pointer border border-destructive
-        ${textColor} ${bgButton} hover:${bgButton} group ${className}`}
+        className={cn(
+          'relative overflow-hidden cursor-pointer border border-destructive group',
+          bgButton,
+          `hover:${bgButton}`,
+          textColor,
+          className
+        )}
         {...props}>
-        <span className={`absolute inset-0 translate-x-[-100%] transition-transform duration-500 ease-in-out 
-        group-hover:translate-x-0 ${bgHover}`}>
+        <span className={cn(
+          'absolute inset-0 translate-x-[-100%] transition-transform duration-500 ease-in-out group-hover:translate-x-0',
+          bgHover
+        )}>
         </span>
-        <span className={`relative flex items-center gap-2 transition-colors duration-500 group-hover:${textHover}`}>
+        <span className={cn(
+          'relative flex items-center gap-2 transition-colors duration-300',
+          `hover:${textHover}`
+        )}>
           {children}
         </span>
       </Button>
